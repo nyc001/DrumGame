d687abf99e4b77c9ae516f84f096876b40ca4f35
Henry.X <henry.y.xie@gmail.com>
2020-09-10 15:07:50 -0400

    Git init, add .gitignore and Procfile

---
+++ public/index.js
@@ -0,0 +1,67 @@
+var buttonList = document.querySelectorAll("button");
+var buttonArray = [...buttonList];
+var sounds = [
+  "crash",
+  "kick-bass",
+  "snare",
+  "tom-1",
+  "tom-2",
+  "tom-3",
+  "tom-4",
+];
+
+buttonArray.forEach((button, index) => {
+  button.addEventListener("click", () => handleClick(index));
+  console.log(this);
+});
+
+document.addEventListener("keydown", logkey);
+
+function logkey(e) {
+  console.log(e.key);
+  console.log(e.code);
+  switch (e.key) {
+    case "w":
+      handleClick(0);
+      toggleFontWhenKey("w");
+      break;
+    case "a":
+      handleClick(1);
+      toggleFontWhenKey("a");
+      break;
+    case "s":
+      handleClick(2);
+      toggleFontWhenKey("s");
+      break;
+    case "d":
+      handleClick(3);
+      toggleFontWhenKey("d");
+      break;
+    case "j":
+      handleClick(4);
+      toggleFontWhenKey("j");
+      break;
+    case "k":
+      handleClick(5);
+      toggleFontWhenKey("k");
+      break;
+    case "l":
+      handleClick(6);
+      toggleFontWhenKey("l");
+      break;
+
+    default:
+      break;
+  }
+}
+
+function toggleFontWhenKey(key) {
+  document.getElementsByClassName(key)[0].classList.toggle("biggerFont");
+  setTimeout(() => {
+    document.getElementsByClassName(key)[0].classList.toggle("biggerFont");
+  }, 300);
+}
+
+function handleClick(soundsIndex) {
+  new Audio("sounds/" + sounds[soundsIndex] + ".mp3").play();
+}
